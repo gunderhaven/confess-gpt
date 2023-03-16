9f397add81050c37e89b32ce4985937fd6525da7
gunderhaven <gunderhaven@gmail.com>
2023-03-16 20:21:09 +0100

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,27 @@
-import { Layout, Text, Page } from '@vercel/examples-ui'
+import React from 'react';
+import { Text, Page } from '@vercel/examples-ui'
 import { Chat } from '../components/Chat'
 
 function Home() {
   return (
-    <Page className="flex flex-col gap-12">
-      <section className="flex flex-col gap-6">
-        <Text variant="h1">OpenAI GPT-3 text model usage example</Text>
-        <Text className="text-zinc-600">
-          In this example, a simple chat bot is implemented using Next.js, API
-          Routes, and OpenAI API.
-        </Text>
-      </section>
+    <React.Fragment>
+      <Page className="flex flex-col gap-12">
+        <section className="flex flex-col gap-6">
+          <Text variant="h1" style={{ fontFamily: 'Garamond' }}>ConfessGPT</Text>
+          <Text className="text-orange-400">
+            ConfessGPT is a chatbot that emulates the role of a priest in the Catholic tradition. It provides a safe and non-judgmental space for you to confess your thoughts and feelings, and offers guidance and support to help you align your actions with your values.
+          </Text>
+        </section>
 
-      <section className="flex flex-col gap-3">
-        <Text variant="h2">AI Chat Bot:</Text>
-        <div className="lg:w-2/3">
-          <Chat />
-        </div>
-      </section>
-    </Page>
+        <section className="flex flex-col gap-3">
+          <Text variant="h2" style={{ fontSize: '1.4rem' }}>You may begin when ready:</Text>
+          <div className="lg:w-2/3">
+            <Chat />
+          </div>
+        </section>
+      </Page>
+    </React.Fragment>
   )
 }
-
-Home.Layout = Layout
 
 export default Home
